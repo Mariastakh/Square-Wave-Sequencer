844a97c557e0eb00b43d2f49f2c8062323003147
Mariastakh <32569878+Mariastakh@users.noreply.github.com>
2019-01-05 13:28:47 +0000

    Update MainComponent.h

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,18 +1,10 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
-
 #pragma once
 
 #include "maximilian.h"
-#include "scalesBank.hpp"
+#include "scaleBank.hpp"
 #include "../JuceLibraryCode/JuceHeader.h"
 
-// Custom Juce Look and Feel class for the Rotary Sliders:
+// Custom Juce Look and Feel class for the Rotary Dials:
 class OtherLookAndFeel : public LookAndFeel_V4
 {
 public:
@@ -36,7 +28,7 @@ public:
 		auto lineW = jmin(8.0f, rad);
 		auto arcRadius = radius - lineW * 0.5f;
 
-		// Circle:
+		// Big circle:
 		float centreX = x + width * 0.5f;
 		float centreY = y + height * 0.5f;
 		float rx = centreX - radius;
@@ -52,7 +44,6 @@ public:
 		// fill:
 		g.setColour(Colours::darkslategrey);
 		g.fillEllipse(rx2, ry2, (rw / 2), (rw / 2));
-		//
 
 		Path backgroundArc;
 		backgroundArc.addCentredArc(width / 2,
@@ -144,99 +135,74 @@ public:
 	void paint(Graphics& g) override;
 	void resized() override;
 
-	// More functions:
+	// Graphics functions:
 	void timerCallback() override;
 	void drawFrame(Graphics& g);
 
+	// Slider and Button callback functions: 
 	void buttonClicked(Button* button) override;
 	void sliderValueChanged(Slider* slider) override;
 	void sliderDragEnded(Slider* slider) override;
-	//void valueChanged(Value* value) override;
-	void addMessageToBuffer(const MidiMessage& message);
-
 	void styleMenuChanged();
 
-	//void setMidiInput(int index);
+private:
+	//==============================================================================
+	
+	// Square Wave Oscillators and Frequencies:
+	maxiOsc osc1, osc2, osc3;
+	double f1 = 0.0;
+	double f2 = 0.0;
+	double f3 = 0.0;
 
+	// Envelopes:
+	vector<maxiEnv> ADSR;
+	vector<double> ADSRout;
+	int envLen = 8000;
+
+	// Midi: 
+	MidiOutput* midiOutput;
+	AudioDeviceManager deviceManager;
+	ComboBox midiOutputList;
+	vector<MidiMessage> midiMessages;
+	vector<int> transposeBy = { 0, 0, 0, 0, 0 };
+	vector<int> chosenNote;
+	vector<bool> noteAlreadyOff = { true, true, true, true, true };
+	vector<int> velocities; // not used.
+	double previous_sum = 0;
+	int playNoteIndex = 0;
+	bool bottomReached = true;
+	bool playNote = false;
+
+	// Key transposition Selection Menu: 
+	ComboBox keyOptions;
+	Oscillator scaleRef;
+	vector<int> key;
+
+	// Graphics:
+	vector<double> sineBuffer;
+	vector<double> bigSineBuffer;
+	int graphicSize;
+	int bigGraphicSize;
+	bool nextSineBlockReady = false;
+	int index = 0;
+	int time = 0;
+
+	// GUI
+	Slider freq1, freq2, freq3, move_all, transpose_1, envelopeLength;
+	OwnedArray<Slider> t_sliders;
 	OwnedArray<Button> buttonsRow1;
 	OwnedArray<Button> buttonsRow2;
 	OwnedArray<Button> buttonsRow3;
 	OwnedArray<Button> buttonsRow4;
 	OwnedArray<Button> buttonsRow5;
-private:
-	//==============================================================================
-	// Your private member variables go here...
-	maxiOsc osc1, osc2, osc3, osc4, osc5, osc6, osc7, osc8, osc9;
-	bool nextSineBlockReady1 = false;
-	bool playNote = false;
-	vector<double> sineBuffer;
-	vector<double>bigSineBuffer;
-	int graphicsCounter = 0;
-	float decay = 50;
-	double previous_sum = 0;
 
-	int playNoteIndex = 0;
-	maxiEnvelope env;
-	vector<maxiEnv> ADSR;
-	vector<double> ADSRout;
-
-	bool waitForPeak = true;
-	bool bottomReached = true;
-	int index = 0;
-	int graphicSize;
-	int bigGraphicSize;
-
-	double f1 = 0.0;
-	double f2 = 0.0;
-	double f3 = 0.0;
-	// GUI
-	Slider freq1, freq2, freq3, move_all, transpose_1, envelopeLength;
-	//vector<vector<TextButton>> noteButtons;
-	vector<vector<bool>> On;
-
-	//=======
-	//vector<TextButton> buttons;
-
-	int envLen = 8000;
-
-	vector<int> chosenNote;
-
-	// For MIDI:
-	AudioDeviceManager deviceManager;           // [1]
-	ComboBox midiOutputList;  // [2]
-	ComboBox midiInputList;
-	Label midiOutputListLabel;
-	int lastOutputIndex = 0;
-	int timer2 = 0;
-	// MIDI Stuff:
-	MidiBuffer midiBuffer;
-	int previousTimeStamp;
-	int time = 0;
-	bool firstLoop = true;
-	MidiOutput* midiOutput;
-	//MidiMessage messageOn;
-	vector<MidiMessage> midiMessages;
-	vector<int> velocities;
-	bool mono = false;
-	vector<bool> noteAlreadyOff = { true, true, true, true, true };
-	OtherLookAndFeel otherLookAndFeel;
-
-	// move all slider:
-	double previousValue = 0;
-
-	// Transpose slider:
-	//int transpose_1_by = 0;
-	OwnedArray<Slider> t_sliders;
-	vector<int> transposeBy = { 0, 0, 0, 0, 0 };
-
-	// Key transposition Combo Box:
-	ComboBox keyOptions;
-	Oscillator scaleRef;
-	vector<int> key;
-
+	// Sequencer Grid: 
 	vector<int> previousState = { 0, 0, 0, 0, 0 };
 	vector<bool> firstButtonClick = { true, true, true, true, true };
 	vector<bool> skipNote = {false, false, false, false, false};
+
+	// Custom Look and Feel
+	OtherLookAndFeel otherLookAndFeel;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
